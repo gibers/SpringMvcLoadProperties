8c08beb1839ffb1a697fdd59bdf2b5371126ac8c
gibers <chbaptis@hotmail.fr>
2016-01-26 23:36:37 +0100

    ajout des fichiers de propriété avec @PropertySource

--- src/main/java/configuration/RootConfig.java
+++ src/main/java/configuration/RootConfig.java
@@ -2,12 +2,15 @@ package configuration;
 
 import java.util.regex.Pattern;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.core.type.filter.RegexPatternTypeFilter;
 
 import configuration.RootConfig.WebPackage;
@@ -15,16 +18,20 @@ import configuration.RootConfig.WebPackage;
 
 @Configuration
 @ImportResource("classpath:myBean.xml")
+@PropertySource({"classpath:com/jbc/monpack/debiere/configFile.properties", "classpath:configFile1.properties"})
 @ComponentScan(
 	excludeFilters={
         @Filter(type=FilterType.CUSTOM, value=WebPackage.class)
     })
 public class RootConfig {
+	
+//	@Autowired
+//	Environment env;
 
 	public static class WebPackage extends RegexPatternTypeFilter {
 		public WebPackage() {
 			super(Pattern.compile("controller"));
-		}    
+		}
 	}
 
 }
--- src/main/java/controller/Totocontroller.java
+++ src/main/java/controller/Totocontroller.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,14 @@ public class Totocontroller {
 	@Autowired
 	MessageSource resources;
 	
+	@Autowired
+	Environment env;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public String test(Model model) {
 
+	    System.out.println(env.getProperty("propriete1"));
+	    System.out.println(env.getProperty("proprieteDuFichier2"));
 
 	    String message = resources.getMessage("valeur1", null, "Default", null);
 	    System.out.println(message);
